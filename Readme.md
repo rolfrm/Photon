340edbd653ecd7acd6913f8c2f40c30e50f57d29
Rolf Madsen <rolfrm@gmail.com>
2015-06-02 19:25:21 +0200

    added readme.md

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Photon
+A c based Lisp compiler.
+
+Only works onl 64bit Linux at the moment.
+
+### To test the compiler.
+1. Check out the code
+2. cd photon
+3. make
+4. ./photon test.lisp
+5. ./photon \#to start the REPL
--- test.lisp
+++ test.lisp
@@ -1,12 +1,10 @@
-2
+;; The following code compiles
+"hello world!"
+(write_line "hello world!")
+
+; It is possible to compile functions
 (defun adds (i64 (a i64) (b i64) (c i64))
   (progn
-    ;(write_line "hello?")
-    
     (i64_add a (i64_add b c))))
 
-(defun superadd (i64)
-  (adds (adds 1 2 3) (adds 3 4 5) (adds 10 20 30)));
-
-(superadd) (superadd) (superadd) (superadd)
-(superadd) (superadd) (superadd) (superadd) (cast "hello" i64)
+(adds 3 4 5) ; this code should print 12
