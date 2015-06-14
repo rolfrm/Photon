10bfd9e7b06ffea820b9bae9e13b93dc8d7f5f73
Rolf Madsen <rolfrm@gmail.com>
2015-06-14 14:45:23 +0200

    forgot to add file

---
+++ c_ast.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iron/types.h>
+#include <iron/utils.h>
+#include <iron/log.h>
+#include <iron/test.h>
+#include <iron/fileio.h>
+#include <iron/mem.h>
+#include "lisp_types.h"
+
+const c_block c_block_empty = {0,0};
