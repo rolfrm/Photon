11083a4f73f35aa0a66ad3d1ec4c4d816460d2d5
Rolf Madsen <rolfrm@gmail.com>
2015-06-14 22:29:34 +0200

    using readline for a better history

--- repl.c
+++ repl.c
@@ -8,17 +8,42 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
-int check_expression(int status, char * buffer){
-  while(*buffer){
-    if(*buffer == '(')
-      status += 1;
-    if(*buffer == ')')
-      status -= 1;
+
+char * read_string(char * buffer){
+  while(*buffer != '"'){
     if(*buffer == '\\')
       buffer++;
+    if(!*buffer)
+      return NULL;
     buffer++;
   }
-  return status;
+  return buffer + 1;
+}
+
+char * read_paren(char * buffer){
+  while(*buffer != ')'){
+    if(*buffer == '"')
+      buffer = read_string(buffer + 1);
+    if(buffer == NULL || !*buffer)
+      return NULL;
+    buffer++;
+  }
+  return buffer + 1;
+}
+
+bool check_expression(char * buffer){
+  while(*buffer){
+    if(*buffer == '(')
+      buffer = read_paren(buffer + 1);
+    else if(*buffer == '"')
+      buffer = read_string(buffer + 1);
+    else if(*buffer ==')')
+      ERROR("illegal symbol");
+    if(buffer == NULL)
+      return false;
+    buffer++;
+  }
+  return true;
 }
 
 bool start_read_eval_print_loop(compiler_state * c){
@@ -26,17 +51,17 @@ bool start_read_eval_print_loop(compiler_state * c){
   char * code = NULL;
   int expr_state = 0;
   while(true){
-    while(code == NULL || 0 != (expr_state = check_expression(expr_state, code))){
+    while(code == NULL || !check_expression(code)){
     
       if(code == NULL){
 	char * new_code = readline("> ");
 	code = fmtstr("%s",new_code);
 	free(new_code);
       }else{
-	char * new_code = readline("> ");
+	char * new_code = readline(" ");
 	char * new2 = fmtstr("%s",new_code);
 	free(new_code);
-	char * new3 = fmtstr("%s%s", code, new2);
+	char * new3 = fmtstr("%s\n%s", code, new2);
 	dealloc(code);
 	dealloc(new2);
 	code = new3;
@@ -47,7 +72,7 @@ bool start_read_eval_print_loop(compiler_state * c){
       ERROR("Invalid paren matching");
       goto reset;
     }
-    
+    add_history(code);
     size_t exprcnt;
     expr * exprs = lisp_parse_all(code, &exprcnt);
     lisp_run_exprs(c, exprs, exprcnt);
