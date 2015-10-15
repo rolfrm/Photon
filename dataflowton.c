753bfac3c43ee890614ceafde25048188995b284
Rolf Madsen <rolfrm@gmail.com>
2015-10-15 17:24:58 +0200

    Playing with types

--- dataflowton.c
+++ dataflowton.c
@@ -1,5 +1,8 @@
 #include<iron/full.h>
 #include "uthash.h"
+#include "lisp_parser.h"
+#include "lisp_types.h"
+type_def * str2type(char * str);
 
 typedef struct {
   void ** key;
@@ -23,23 +26,54 @@ void ensure_size(void ** buffer, size_t min_size){
   } 
 }
 
-size_t calc_norm2(double * in_xes, double * in_yes, double * in_zes, size_t cnt, double ** output){
-  ensure_size((void **) output, cnt * sizeof(double));
-  double * o = *output;
+void ensure_size2(void ** buffer, size_t * size, size_t min_size){
+  if(size < min_size){
+    *buffer = ralloc(*buffer, min_size);
+    *size = min_size;
+  } 
+}
+typedef struct{
+  type_def * fcn_type;
+  void * fcn;
+  
+}node_type;
+
+typedef struct{
+  node_type type;
+  void (* destroy) (node_base * node);
+}node_base;
+
+typedef struct{
+  node_base base;
+  double * buffer;
+  u64 buffer_size;
+}norm2_data;
+
+size_t calc_norm2(double * in_xes, double * in_yes, double * in_zes, size_t cnt, norm2_data * output){
+  ensure_size2((void **) &output->buffer, &output->buffer_size, cnt * sizeof(double));
+  double * o = output->buffer;
   for(size_t i = 0; i < cnt; i++){
     o[i] = sqrt(in_xes[i] * in_xes[i] + in_yes[i] * in_yes[i] + in_zes[i] * in_zes[i]);
-    logd("%f\n", o[i]);
   }
   return cnt;
 }
 
-size_t calc_delay_line(double * in, size_t cnt, double ** out, double ** delay_buffer){
-  ensure_size((void **) out, cnt * sizeof(double));
-  if(*delay_buffer == NULL)
-    *delay_buffer = alloc0(sizeof(double));
+typedef struct{
+  node_base base;
+  double * buffer;
+  u64 buffer_size;
+  double * delay_buffer;
+  i64 delay_size;
+}delay_data;
+
+size_t calc_delay_line(double * in, size_t cnt, double ** out, delay_data * delay_buffer){
+  ensure_size2((void **) &delay_buffer->buffer, &delay_buffer->buffer_size, cnt * sizeof(double));
+  ensure_size2((void **) &delay_buffer->delay_buffer, &delay_buffer->delay_size, delay_buffer->delay_size);
+  
   double * o = *out;
   for(size_t i = 0; i < cnt; i++){
-    o[i] = **delay_buffer;
+    //??
+    o[i] = delay_buffer->delay_buffer;
     **delay_buffer = in[i];
   }
   return cnt;
@@ -65,8 +99,14 @@ size_t add_line(const double * a, const double * b, size_t cnt, double ** o){
     (*o)[i] = a[i] + b[i];
   return cnt;
 }
+
 void run_http_serv();
+void load_defs();
 bool test_dataflow(){
+  load_defs();
+  type_def * t = str2type("(fcn u64 (frequency f64) (phase f64) (cnt u64) (output (ptr (ptr f64))))");
+  // this can be used to describe types to the system.
+  
   run_http_serv();
   while(true)
     usleep(100000);
